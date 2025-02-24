208fbef23141de1da3210bd9694bcd256dc1d961
coyaks19 <coyaks19@gmail.com>
2025-02-24 03:28:08 -0500

    first

---
+++ src/main/java/com/skoy/bootcamp_microservices/BootcampMicroservicesApplication.java
@@ -0,0 +1,13 @@
+package com.skoy.bootcamp_microservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootcampMicroservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootcampMicroservicesApplication.class, args);
+	}
+
+}
